6ca8b4cb19613a3645590eb4170e1c3cdf65bc24
jiyang2211 <jiyang2211@gmail.com>
2022-08-31 12:03:07 +0900

    08-31 거꾸로코딩수업 - 자료형, 산술/비교 연산자

---
+++ 08-31/index.js
@@ -0,0 +1,49 @@
+var a = 3;
+// 카멜방식
+let b = 3;
+const c = 3;
+// console.log('a:', a);
+// console.log('b:',b);
+// console.log('c:',c);
+a=5;
+var aPlusB = a+b;
+console.log(aPlusB);
+
+console.log('a+b = ', a+b);
+console.log('a-b = ', a-b);
+console.log('a*b = ', a*b);
+console.log('a/b = ', a/b);
+console.log('a%b = ', a%b);
+
+a+=4;
+
+console.log('a를 9로 만드는 방법', a);
+console.log('a-=4', a-=4);
+console.log('a*=4', a*=4);
+console.log('a/=4', a/=4);
+
+var e = 3;
+var result = e++;
+console.log('result = ', result); // result = 결과
+console.log('e = ', e); 
+
+var f = 3;
+var result2 = ++f;
+console.log('result2 = ', result2); // result = 결과
+console.log('f = ', f); 
+
+var g = true;
+var h = false;
+console.log('g:',g);
+console.log('h:',h);
+
+var aa = 3;
+var bb = 3;
+
+console.log('aa < bb = ', aa<bb);
+console.log('aa > bb = ', aa>bb);
+console.log('aa <= bb = ', aa<=bb);
+console.log('aa >= bb = ', aa>=bb);
+console.log('aa == bb = ', aa==bb); // '===' 타입도 같아야함
+console.log('aa != bb = ', aa!=bb);
+
