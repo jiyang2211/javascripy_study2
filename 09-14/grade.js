467ddbf4d8e7137d47c14fbe8d2e23b7c4ef451c
jiyang2211 <jiyang2211@gmail.com>
2022-09-14 12:00:54 +0900

    9월 14일 if문 실습

--- 09-14/grade.js
+++ 09-14/grade.js
@@ -9,6 +9,7 @@ if(a==100){
 } else if(a<=70 && a>=79){
     console.log("B 입니다");
 } else if(a<=60 && a>=69){
+    // 60 <= a <= 69
     console.log("C+ 입니다");
 } else if(a<=50 && a>=59){
     console.log("C 입니다");
